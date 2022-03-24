ca93f9787ecbb424fdfa03e76069c1b021d99e1c
IVovchynskyi <vovchinsky7898@gmail.com>
2022-03-25 01:34:04 +0200

    HW1: 1.1 is done

--- HW1.py
+++ HW1.py
@@ -16,3 +16,27 @@ dict_two = { ‘a’: 6,  ‘b’: 7, ‘z’: 20, ‘x’: 40 }
 keys = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 ожидаемый результат: {1: 1, 2: 4, 3: 9 …}
 """
+
+# 1.
+InitialArray = [10, 11, 2, 3, 5, 8, 23, 11, 2, 5, 76, 43, 2, 32, 76, 3, 10, 0, 1]
+# убрать из него повторяющиеся элементы
+NoDuplicateArray = list(set(InitialArray))
+# print(NoDuplicateArray)
+
+# The function to check that we were right
+def DuplicateInArray(TheArray):
+    while bool(TheArray) is True:
+        ComparisonTarget = TheArray.pop()
+        # print (ComparisonTarget)
+        # print(TheArray)
+        for element in TheArray:
+            # print(f"comparing {ComparisonTarget} with {element}")
+            if ComparisonTarget == element:
+                print(f"{ComparisonTarget} is the duplicate!")
+                return
+            # print("--------------")
+    print("No duplicates were found")
+# Checkign process
+DuplicateInArray(InitialArray)
+
+
