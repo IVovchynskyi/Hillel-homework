530d7801d5a5a6d1a8dc5fd78b71c407b9dbea2c
IVovchynskyi <vovchinsky7898@gmail.com>
2022-04-09 15:48:53 +0300

    HW4: the first tat is done[C

--- HW4.py
+++ HW4.py
@@ -10,6 +10,9 @@
 # Если это int, тогда выполнить функцию и вывести результат, если это str(),
 # тогда зарейзить ошибку ValueError (raise ValueError(“string type is not supported”))
 
+Привет, по поводу задачи 2 в декораторах. По условию вы должны там рейзить ошибку, не смущайтесь когда вылетит трейсбек
+с ошибкой  ValueError: string type is not supported. Так и задуманно
+
 Пример рейза ошибок:
 try:
   f()
@@ -23,3 +26,29 @@ except ValueError:
 # Если значение берется из переменной cache, вывести сообщение «Used cache with counter = {}» и
 # количество раз обращений в cache.
 """
+
+# ЗАДАЧА-1
+# Написать свой декоратор который будет проверять остаток от деления числа 100 на результат работы функции.
+# Если остаток от деления = 0, вывести сообщение "We are OK!», иначе «Bad news guys, we got {}» остаток от деления.
+
+
+def decorator_division_reminder(division_reminder):
+    def wrapper_around_original_function(*args, **kwargs):
+        if division_reminder(*args, **kwargs) == 0:
+            print("We are OK!")
+        else:
+            print(f"Bad news guys, we got {division_reminder(*args, **kwargs)}")
+
+    return(wrapper_around_original_function)
+
+@decorator_division_reminder
+def division_reminder(dividend, divisor):
+    """
+    a test function to study decorators
+    :param dividend: the number to be divided (e.g. 7 in 7 / 5)
+    :param divisor: the number that divides (e.g. 5 in 7 / 5)
+    :return: the remainder after dividing the dividend by the divisor (e.g. 2 in 7 / 5)
+    """
+    return dividend % divisor
+
+
