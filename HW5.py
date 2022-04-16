a970932f8ddbb0417aea4180db13758844cc1b58
IVovchynskyi <vovchinsky7898@gmail.com>
2022-04-17 01:29:29 +0300

    HW5: initial commit

---
+++ HW5.py
@@ -0,0 +1,11 @@
+"""
+1) Реализовать класс который бы производил разные операции над 2мя переданными числами.
+
+Пример Использования:
+
+proccess_input = ProcessInput(a=20, b=10)
+print(proccess_input.add()). # выведет 30
+print(proccess_input.subtract()). # выведет 10
+print(proccess_input.multiple()). # выведет 200
+print(proccess_input.divide()). # выведет 2
+"""
