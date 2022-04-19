3714ff42b3e07a43e47e7d90f5369db4234168dc
IVovchynskyi <vovchinsky7898@gmail.com>
2022-04-20 01:38:16 +0300

    HW5: typeerror correction added

--- HW5.py
+++ HW5.py
@@ -10,45 +10,50 @@ print(proccess_input.multiple()). # выведет 200
 print(proccess_input.divide()). # выведет 2
 """
 
-
+from typing import Union
 class ProcessInput:
     """
     the class to perform some actions with two input digits
     """
-
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
-        # try:
-        #     if isinstance(a, (float, int)) and isinstance(b, (float, int)):
-        #         self.a = a
-        #         self.b = b
-        # except:
-
+    def __init__(self, a: Union[int, float], b: Union[int, float]):
+       self.a = a
+       self.b = b
 
     def add(self):
-        return self.a + self.b
-
+        try:
+            return self.a + self.b
+        except TypeError:
+            print("Cannot add: some of your input data belong to a wrong type (not int nor float)")
     def subtract(self):
-        return self.a - self.b
+        try:
+            return self.a - self.b
+        except TypeError:
+            print("Cannot subtract: some of your input data belong to a wrong type (not int nor float)")
 
     def multiple(self):
-        return self.a * self.b
+        try:
+            return self.a * self.b
+        except TypeError:
+            print("Cannot multiply: some of your input data belong to a wrong type (not int nor float)")
 
     def divide(self):
         try:
             return self.a / self.b
         except ZeroDivisionError:
             print("Fortunately or unfortunately, but you cannot devide by a zero")
+        except TypeError:
+            print("Cannot devide: some of your input data belong to a wrong type (not int nor float)")
     def divide_v2(self):
         try:
             return int(self.a / self.b)
+        except TypeError:
+            print("Cannot devide: some of your input data belong to a wrong type (not int nor float)")
         except ZeroDivisionError:
             print("Fortunately or unfortunately, but you cannot devide by a zero")
 
 
 
-proccess_input = ProcessInput(a=20, b=0)
+proccess_input = ProcessInput(a='1.1', b=0)
 print(proccess_input.add()) # выведет 30
 print(proccess_input.subtract()) # выведет 10
 print(proccess_input.multiple()) # выведет 200
