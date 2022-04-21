fc1be78ec5b2d40d02c0f318cfc8470b1a3130c4
IVovchynskyi <vovchinsky7898@gmail.com>
2022-04-21 05:03:23 +0300

    HW6: 2.1 and 2.3 are done

--- HW6.py
+++ HW6.py
@@ -25,12 +25,14 @@ ip_proccessing = IpProccessing(ip_adresses)
 files_processing = FilesProccessing(file_path, mode)
 """
 
+
 class IpProccessing:
     """
     list of IP addresses to be processed in various ways
     """
+
     def __init__(self, ips):
-       self.ips = ips
+        self.ips = ips
 
     def revers(self):
         revers_list = []
@@ -54,7 +56,7 @@ class IpProccessing:
         return last_only_list
 
 
-ip_adresses = ["10.11.12.13", "10.11.12.14", "10.11.12.15"]
+ip_adresses = ["10.11,12.13", "10a.11.12.14", "10.11.12.15"]
 ip_proccessing = IpProccessing(ip_adresses)
 
 # 1) Получить список IP адресов в развернутом виде
@@ -66,3 +68,41 @@ print(ip_proccessing.no_first())
 # 3) Получить список последних октетов IP адресов
 # (пример 10.11.12.13 -> 13)
 print(ip_proccessing.last_only())
+
+
+# I have made a cool "validator", but I cannot use it in the class element initiation, and I am a bit upset about it
+# for elem in ["10.11.12.13", "10.11.12.a4", "10,11,12,15"]:
+#     if elem.count('.') == 3 and all(i.isdigit() for i in elem.split('.')):
+#         print("IP seems to be fine")
+#     else:
+#         print("Something wrong with your IP")
+
+
+class FilesProccessing:
+    """
+    it gets the path and the mode as an input
+    mode = a means add if file exist
+    mode = w means re-write if file exist
+    """
+
+    def __init__(self, file_path, mode):
+        self.file_path = file_path
+        self.mode = mode
+
+    def write_it(self, text: str):
+        if self.mode == 'w' or self.mode == 'a':
+            text = str(text)  # I have checked; srt() can get RuntimeError or MemoryError only, so I don't need try/except
+            with open(self.file_path, self.mode) as file:
+                file.write(text + '\n')
+        else:
+            print(f"Entered mode {self.mode} is not 'w' or 'a'")
+            return
+
+    def eraste_it(self):
+        with open(self.file_path, 'w') as file:
+            return
+
+
+files_processing = FilesProccessing('./test.txt', 'a')
+files_processing.write_it('puppe1')
+files_processing.eraste_it()
