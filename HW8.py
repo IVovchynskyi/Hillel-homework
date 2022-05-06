794aa0aeda28eb484dded67caf6148893621cec4
IVovchynskyi <vovchinsky7898@gmail.com>
2022-05-07 01:57:09 +0300

    the task is done

--- HW8.py
+++ HW8.py
@@ -1,36 +1,26 @@
 """
 Создать тест в котором происходит инициализация драйвера.
 Перейдите на любой выбранный вами ресурс. Нажмите на копку либо введите текст в поле ввода.
-----
-Chrome is up to date
-Version 101.0.4951.54 (Official Build) (64-bit)
------
-chromedriver_win32_(101_0_4951_141) https://chromedriver.storage.googleapis.com/index.html?path=101.0.4951.41/
 """
 
 from selenium import webdriver
-
-
-#  options
-
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from time import sleep as sleep
+from time import sleep
 
-
-# options = Options()
 options = webdriver.ChromeOptions()
-
-# options.add_argument("start-maximized")
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 try :
     driver.get("https://incompetech.com/wordpress/")
+    sleep(1)
+    search_field = '//*[@id="s"]'
+    element = driver.find_element(By.XPATH, search_field)
+    element.clear() # just in case
+    element.send_keys("baba yaga")
+    element.send_keys(Keys.ENTER) # using ENTER
     sleep(3)
 finally:
     driver.close()
