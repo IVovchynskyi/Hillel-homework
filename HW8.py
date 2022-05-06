2174fab7552c683834bf9ea8c46ab12f8a2e6931
IVovchynskyi <vovchinsky7898@gmail.com>
2022-05-07 01:28:02 +0300

    initial commit

---
+++ HW8.py
@@ -0,0 +1,10 @@
+"""
+Создать тест в котором происходит инициализация драйвера.
+Перейдите на любой выбранный вами ресурс. Нажмите на копку либо введите текст в поле ввода.
+----
+Chrome is up to date
+Version 101.0.4951.54 (Official Build) (64-bit)
+-----
+chromedriver_win32_(101_0_4951_141) https://chromedriver.storage.googleapis.com/index.html?path=101.0.4951.41/
+"""
+
