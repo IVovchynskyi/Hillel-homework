eb5218bd50afd9f698bc6b6f6181cb6cbbd35911
IVovchynskyi <vovchinsky7898@gmail.com>
2022-05-22 00:24:52 +0300

    login as palin code ready

--- hw9_main.py
+++ hw9_main.py
@@ -10,7 +10,10 @@ options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 
-# url = 'http://httpbin.org/status/404'
+
+
+
+
 url = "https://www.aqa.science/"
 
 try:
@@ -26,7 +29,25 @@ except requests.exceptions.RequestException as err:
 else:
     driver.get(url)
     sleep(1)
+    login_button = '/html/body/div/div[1]/div/ul/li/a'
+    element = driver.find_element(By.XPATH, login_button)
+    element.click()
+    sleep(1)
+    login_username = '//*[@id="id_username"]'
+    login_password = '//*[@id="id_password"]'
+    login_sumit_button = '//*[@id="submit-id-submit"]'
+    # 3 lines below need to be replaced by "enter-text-function-byXPATH"
+    element = driver.find_element(By.XPATH, login_username)
+    element.clear() # just in case
+    element.send_keys("admin") # need to be more secure
 
+    element = driver.find_element(By.XPATH, login_password)
+    element.clear() # just in case
+    element.send_keys("admin123") # need to be more secure
+
+    element = driver.find_element(By.XPATH, login_sumit_button)
+    element.click()
+    sleep(3)
 finally:
     driver.close()
     driver.quit()
