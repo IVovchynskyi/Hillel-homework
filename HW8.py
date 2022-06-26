f00a9dcf150a8dca52c49120b467cd8a81e0a0be
IVovchynskyi <vovchinsky7898@gmail.com>
2022-06-26 03:44:09 +0300

    try-except-finaly is useed properly; the code is closer to the test now

--- HW8.py
+++ HW8.py
@@ -8,22 +8,43 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import requests
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 from time import sleep
 
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-try :
-    driver.get("https://incompetech.com/wordpress/")
-except ZeroDivisionError: # I cannot use else without except (still can finally, but not else)
-    pass
+MAIN_URL = "https://incompetech.com/wordpress/"
+
+try:
+    requests.get(MAIN_URL, timeout=3).raise_for_status()
+except requests.exceptions.HTTPError as errh:
+    print("Http Error:", errh)
+except requests.exceptions.ConnectionError as errc:
+    print("Error Connecting:", errc)
+except requests.exceptions.Timeout as errt:
+    print("Timeout Error:", errt)
+except requests.exceptions.RequestException as err:
+    print("OOps: Something Else", err)
 else:
-    sleep(1)
+    driver.get(MAIN_URL)
+    sleep(2)
     search_field = '//*[@id="s"]'
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, search_field)))
     element = driver.find_element(By.XPATH, search_field)
     element.clear() # just in case
     element.send_keys("baba yaga")
     element.send_keys(Keys.ENTER) # using ENTER
-    sleep(3)
+    sleep(2)
+    try:
+        baba_yaga_link = '//*[@id="post-1569"]/h2/a'
+        driver.find_element(By.XPATH, baba_yaga_link)
+    except NoSuchElementException:
+        print("The link is absent!")
+    else:
+        print("The link is here. It is fine")
 finally:
     driver.close()
     driver.quit()
